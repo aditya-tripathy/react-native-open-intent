b30faf4cb44d15f112d561d87f78ab41bdd806d3
Aditya Kumar Tripathy <aditya.tripathy@shopx.in>
2021-04-29 14:50:03 +0530

    changes done

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 # react-native-open-intent
 
+This package covers intent based operations like UPI Payment in android, Open Deeplink of UPI apps, Other Intent based operations. It also support All Installed Apps. System apps and Non System apps. 
+
+You can also check a paticular app is installed or not using package name.
+
 ## Getting started
 
 `$ npm install react-native-open-intent --save`
 
 ### Mostly automatic installation
 
-`$ react-native link react-native-open-intent`
+`$ react-native link react-native-open-intent` for rn 0.59 or  lower.
+
+## Methods
+
+#### 1 - openLink(url, packageName)
+#### 2 - isPackageInstalled()
+#### 3 - getApps()
+#### 4 - getNonSystemApps()
+#### 5 - getSystemApps()
 
 ## Usage
 ```javascript
@@ -52,7 +64,7 @@ export const parseResponse = (response) => {
     return responseData;
 };
 
-//ex: upi://pay?pa=example@yesbank&pn=Example%20Merchant&mc=&tr=3156431614262946822&tn=Paying%20to%20ShopX&am=1000.0&mam=0&cu=INR&url=
+let UpiUrl = 'upi://pay?pa=example@yesbank&pn=Example%20Merchant&mc=&tr=3156431614262946822&tn=Paying%20to%20adi&am=1000.0&mam=0&cu=INR&url=';
 
 const paymentHandler = async (UpiUrl) => {
     let response = null;
@@ -64,7 +76,21 @@ const paymentHandler = async (UpiUrl) => {
 };
 ```
 
-### To check using package name
+
+If You want to open a paticular apps send the package name in seccond param. Below example I have opened phonepe you can use any other apps. It supports all available upi app.
+
+```javascript
+const paymentHandler = async (UpiUrl, 'com.phonepe.app') => {
+    let response = null;
+    try {
+        response = parseResponse(await OpenIntent.openLink(UpiUrl));
+    } catch (err) {
+        response = parseResponse(null);
+    }
+};
+```
+
+### To check a app is installed or not
 
 ```javascript
 
@@ -73,4 +99,39 @@ OpenIntent.isPackageInstalled('com.whatsapp', (isInstalled) => {
     // isInstalled is true if the app is installed or false if not
 });
 
+```
+
+### Get All System apps
+
+```javascript
+let allInstalledApps = await OpenIntent.getApps();
+console.log('allInstalledApps: ', allInstalledApps);
+
+[
+  {
+    "apkDir": "/data/app/com.healthkart.healthkart-zDMq_ERadbJNekV7XGMDZQ==/base.apk",
+    "appName": "HealthKart",
+    "firstInstallTime": 1578846224181,
+    "icon": "iVBORw0KGgoAAAANSUhEUgAAAKgAAACoCAYAAAB0S6W0AAAAAX",
+    "lastUpdateTime": 1619112973398,
+    "packageName": "com.healthkart.healthkart",
+    "size": 15552800,
+    "versionCode": 339,
+    "versionName": "16.1.1"
+  }
+]
+```
+
+### Get All Installed system apps
+
+```javascript
+let apps = await OpenIntent.getSystemApps();
+console.log('apps: ', apps);
+```
+
+### Get All Installed non system apps
+
+```javascript
+let apps = await OpenIntent.getNonSystemApps();
+console.log('apps: ', apps);
 ```
--- android/src/main/java/com/thronie/intent/OpenIntentModule.java
+++ android/src/main/java/com/thronie/intent/OpenIntentModule.java
@@ -11,8 +11,22 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.Arguments;
+
 import android.content.pm.PackageManager;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.ApplicationInfo;
+import android.graphics.drawable.Drawable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.io.File;
+
 
 public class OpenIntentModule extends ReactContextBaseJavaModule {
 
@@ -33,15 +47,17 @@ public class OpenIntentModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void openLink(String url, final Promise promise) {
+    public void openLink(String url, String packageName, final Promise promise) {
         // TODO: Implement some actually useful functionality
         intentPromise = promise;
         Uri uri = Uri.parse(url);
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-
+        if(packageName != null) {
+            intent.setPackage(packageName);
+        }
         try {
             this.reactContext.startActivityForResult(intent, 5864, null);
-        } catch (Exception e){
+        } catch (Exception e) {
             intentPromise.reject("Failed", "NO APP FOUND");
         }
     }
@@ -55,6 +71,117 @@ public class OpenIntentModule extends ReactContextBaseJavaModule {
         } catch (Exception e) {
             cb.invoke(false);
         }
+    }
+
+    @ReactMethod
+    public void getApps(Promise promise) {
+        try {
+            PackageManager pm = this.reactContext.getPackageManager();
+            List < PackageInfo > pList = pm.getInstalledPackages(0);
+            WritableArray list = Arguments.createArray();
+            for (int i = 0; i < pList.size(); i++) {
+                PackageInfo packageInfo = pList.get(i);
+                WritableMap appInfo = Arguments.createMap();
+
+                appInfo.putString("packageName", packageInfo.packageName);
+                appInfo.putString("versionName", packageInfo.versionName);
+                appInfo.putDouble("versionCode", packageInfo.versionCode);
+                appInfo.putDouble("firstInstallTime", (packageInfo.firstInstallTime));
+                appInfo.putDouble("lastUpdateTime", (packageInfo.lastUpdateTime));
+                appInfo.putString("appName", ((String) packageInfo.applicationInfo.loadLabel(pm)).trim());
+
+                Drawable icon = pm.getApplicationIcon(packageInfo.applicationInfo);
+                appInfo.putString("icon", Utility.convert(icon));
+
+                String apkDir = packageInfo.applicationInfo.publicSourceDir;
+                appInfo.putString("apkDir", apkDir);
+
+                File file = new File(apkDir);
+                double size = file.length();
+                appInfo.putDouble("size", size);
+
+                list.pushMap(appInfo);
+            }
+            promise.resolve(list);
+        } catch (Exception ex) {
+            promise.reject(ex);
+        }
+    }
+
+    @ReactMethod
+    public void getNonSystemApps(Promise promise) {
+        try {
+            PackageManager pm = this.reactContext.getPackageManager();
+            List < PackageInfo > pList = pm.getInstalledPackages(0);
+            WritableArray list = Arguments.createArray();
+            for (int i = 0; i < pList.size(); i++) {
+                PackageInfo packageInfo = pList.get(i);
+                WritableMap appInfo = Arguments.createMap();
+
+                if ((packageInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
+                    appInfo.putString("packageName", packageInfo.packageName);
+                    appInfo.putString("versionName", packageInfo.versionName);
+                    appInfo.putDouble("versionCode", packageInfo.versionCode);
+                    appInfo.putDouble("firstInstallTime", (packageInfo.firstInstallTime));
+                    appInfo.putDouble("lastUpdateTime", (packageInfo.lastUpdateTime));
+                    appInfo.putString("appName", ((String) packageInfo.applicationInfo.loadLabel(pm)).trim());
+
+                    Drawable icon = pm.getApplicationIcon(packageInfo.applicationInfo);
+                    appInfo.putString("icon", Utility.convert(icon));
+
+                    String apkDir = packageInfo.applicationInfo.publicSourceDir;
+                    appInfo.putString("apkDir", apkDir);
+
+                    File file = new File(apkDir);
+                    double size = file.length();
+                    appInfo.putDouble("size", size);
+
+                    list.pushMap(appInfo);
+                }
+            }
+            promise.resolve(list);
+        } catch (Exception ex) {
+            promise.reject(ex);
+        }
+
+    }
+
+    @ReactMethod
+    public void getSystemApps(Promise promise) {
+        try {
+            PackageManager pm = this.reactContext.getPackageManager();
+            List < PackageInfo > pList = pm.getInstalledPackages(0);
+            WritableArray list = Arguments.createArray();
+            for (int i = 0; i < pList.size(); i++) {
+                PackageInfo packageInfo = pList.get(i);
+                WritableMap appInfo = Arguments.createMap();
+
+                if ((packageInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
+                    appInfo.putString("packageName", packageInfo.packageName);
+                    appInfo.putString("versionName", packageInfo.versionName);
+                    appInfo.putDouble("versionCode", packageInfo.versionCode);
+                    appInfo.putDouble("firstInstallTime", (packageInfo.firstInstallTime));
+                    appInfo.putDouble("lastUpdateTime", (packageInfo.lastUpdateTime));
+                    appInfo.putString("appName", ((String) packageInfo.applicationInfo.loadLabel(pm)).trim());
+
+                    Drawable icon = pm.getApplicationIcon(packageInfo.applicationInfo);
+                    appInfo.putString("icon", Utility.convert(icon));
+
+                    String apkDir = packageInfo.applicationInfo.publicSourceDir;
+                    appInfo.putString("apkDir", apkDir);
+
+                    File file = new File(apkDir);
+                    double size = file.length();
+                    appInfo.putDouble("size", size);
+
+                    list.pushMap(appInfo);
+                }
+            }
+            promise.resolve(list);
+        } catch (Exception ex) {
+            promise.reject(ex);
+        }
+
     }
 
     private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
