25badf1f08798eb2bbce7e70d62a22e10be0c887
Aditya Kumar Tripathy <aditya.tripathy@shopx.in>
2021-06-03 11:48:04 +0530

    changes added

--- android/src/main/java/com/thronie/intent/OpenIntentModule.java
+++ android/src/main/java/com/thronie/intent/OpenIntentModule.java
@@ -52,7 +52,7 @@ public class OpenIntentModule extends ReactContextBaseJavaModule {
         intentPromise = promise;
         Uri uri = Uri.parse(url);
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        if(packageName != null) {
+        if (packageName != null) {
             intent.setPackage(packageName);
         }
         try {
@@ -110,40 +110,12 @@ public class OpenIntentModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void getNonSystemApps(Promise promise) {
-        try {
-            PackageManager pm = this.reactContext.getPackageManager();
-            List < PackageInfo > pList = pm.getInstalledPackages(0);
-            WritableArray list = Arguments.createArray();
-            for (int i = 0; i < pList.size(); i++) {
-                PackageInfo packageInfo = pList.get(i);
-                WritableMap appInfo = Arguments.createMap();
+        collectNonSystemApps(true, promise);
+    }
 
-                if ((packageInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
-                    appInfo.putString("packageName", packageInfo.packageName);
-                    appInfo.putString("versionName", packageInfo.versionName);
-                    appInfo.putDouble("versionCode", packageInfo.versionCode);
-                    appInfo.putDouble("firstInstallTime", (packageInfo.firstInstallTime));
-                    appInfo.putDouble("lastUpdateTime", (packageInfo.lastUpdateTime));
-                    appInfo.putString("appName", ((String) packageInfo.applicationInfo.loadLabel(pm)).trim());
-
-                    Drawable icon = pm.getApplicationIcon(packageInfo.applicationInfo);
-                    appInfo.putString("icon", Utility.convert(icon));
-
-                    String apkDir = packageInfo.applicationInfo.publicSourceDir;
-                    appInfo.putString("apkDir", apkDir);
-
-                    File file = new File(apkDir);
-                    double size = file.length();
-                    appInfo.putDouble("size", size);
-
-                    list.pushMap(appInfo);
-                }
-            }
-            promise.resolve(list);
-        } catch (Exception ex) {
-            promise.reject(ex);
-        }
-
+    @ReactMethod
+    public void getNonSystemApps(boolean iconRequired, Promise promise) {
+        collectNonSystemApps(iconRequired, promise);
     }
 
     @ReactMethod
@@ -182,6 +154,44 @@ public class OpenIntentModule extends ReactContextBaseJavaModule {
             promise.reject(ex);
         }
 
+    }
+
+    private void collectNonSystemApps(boolean iconRequired, Promise promise) {
+        try {
+            PackageManager pm = this.reactContext.getPackageManager();
+            List < PackageInfo > pList = pm.getInstalledPackages(0);
+            WritableArray list = Arguments.createArray();
+            for (int i = 0; i < pList.size(); i++) {
+                PackageInfo packageInfo = pList.get(i);
+                WritableMap appInfo = Arguments.createMap();
+
+                if ((packageInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
+                    appInfo.putString("packageName", packageInfo.packageName);
+                    appInfo.putString("versionName", packageInfo.versionName);
+                    appInfo.putDouble("versionCode", packageInfo.versionCode);
+                    appInfo.putDouble("firstInstallTime", (packageInfo.firstInstallTime));
+                    appInfo.putDouble("lastUpdateTime", (packageInfo.lastUpdateTime));
+                    appInfo.putString("appName", ((String) packageInfo.applicationInfo.loadLabel(pm)).trim());
+
+                    if (iconRequired) {
+                        Drawable icon = pm.getApplicationIcon(packageInfo.applicationInfo);
+                        appInfo.putString("icon", Utility.convert(icon));
+                    }
+
+                    String apkDir = packageInfo.applicationInfo.publicSourceDir;
+                    appInfo.putString("apkDir", apkDir);
+
+                    File file = new File(apkDir);
+                    double size = file.length();
+                    appInfo.putDouble("size", size);
+
+                    list.pushMap(appInfo);
+                }
+            }
+            promise.resolve(list);
+        } catch (Exception ex) {
+            promise.reject(ex);
+        }
     }
 
     private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
